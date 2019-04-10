a5b0196e9a652ac61db2e15530e63383633a0a5c
Johnny Kim <youngbeanies@gmail.com>
2019-04-09 22:42:37 -0700

    Completed heap class.

--- heap.hpp
+++ heap.hpp
@@ -4,10 +4,66 @@
 
 #ifndef HEAP_HEAP_HPP
 #define HEAP_HEAP_HPP
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
 
-//<typename T>
+using namespace std;
+
+template <typename containerOfElements, typename T>
 
 class heap{
+
+    deque<T> heap_deque;
+
+    void heapify(){
+        make_heap(heap_deque.begin(), heap_deque.end());
+    }
+
+public:
+
+    heap(containerOfElements items){
+        for(auto item : items){
+            heap_deque.push_back(item);
+        }
+        heapify();
+    }
+
+    void push(T a){
+        heap_deque.push_back(a);
+        heapify();
+    }
+
+    T pop(){
+        T temp = heap_deque.front();
+        heap_deque.pop_front();
+        heapify();
+        return temp;
+    }
+
+    int size(){
+        return (int) heap_deque.size();
+    }
+
+    bool is_empty(){
+        return heap_deque.size() == 0;
+    }
+
+    void clear(){
+        heap_deque.clear();
+    }
+
+private:
+
+    friend ostream& operator<< (ostream& os, const heap& heap){
+        os << "Heap:" << endl;
+        for(int i = 0; i < heap.heap_deque.size(); i++){
+            os << heap.heap_deque[i] << " ";
+        }
+        return os;
+    }
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include "heap.hpp"
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    vector<int> v;
+    v.push_back(5);
+    v.push_back(9);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(8);
+
+    heap<vector<int>, int> hospital(v);
+
+    cout << hospital << endl;
+
+    hospital.pop();
+    hospital.push(10);
+
+    cout << hospital << endl;
+
+    cout << hospital.size() << endl;
+    cout << hospital.is_empty() << endl;
+    hospital.clear();
+    cout << hospital.is_empty() << endl;
+
     return 0;
 }
